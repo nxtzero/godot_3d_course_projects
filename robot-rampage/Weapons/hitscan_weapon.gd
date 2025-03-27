64c48f29c279c5f2c9d35beca171a2251c0aec71
NxtZero <nxtzero@gmail.com>
2025-03-27 15:51:07 -0100

    lesson 82 - gun always visible

--- robot-rampage/Weapons/hitscan_weapon.gd
+++ robot-rampage/Weapons/hitscan_weapon.gd
@@ -5,6 +5,7 @@ extends Node3D
 @export var weapon_mesh: Node3D
 @export var weapon_damage := 15
 @export var muzzle_flash: GPUParticles3D
+@export var sparks: PackedScene
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 @onready var cooldown_timer: Timer = $CooldownTimer
@@ -25,3 +26,6 @@ func shoot() -> void:
 	weapon_mesh.position.z += recoil
 	if collider is Enemy:
 		collider.hitpoints -= weapon_damage
+	var spark = sparks.instantiate()
+	add_child(spark)
+	spark.global_position = ray_cast_3d.get_collision_point()
